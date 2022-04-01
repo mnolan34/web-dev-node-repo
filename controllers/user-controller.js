88e9a21f6b4b4ef7d88008fd2eb3077b6b6b9d13
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-01 13:00:06 -0400

    Implemented 3.2

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,7 +5,18 @@ const userController = (app) => {
     app.get('/api/users', findAllUsers);
 }
 
+
+const findUsersByType =  (type) => {
+    const wantedUsers = users.filter(user => user.type === type);
+    return wantedUsers;
+}
+
 const findAllUsers = (req, res) => {
+    const type = req.query.type;
+    if(type){
+        res.json(findUsersByType(type));
+        return;
+    }
     res.json(users);
 }
 
