dc3b269d5bf7075633d1ce94398a616442947c25
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-01 11:38:06 -0400

    Section 3.1 complete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/user-controller.js";
 const app = express();
 
 helloController(app);
+userController(app);
 app.listen(4000);
