2f744cc75a476580d5a494bd5bac4cc70ac3caf4
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-05 23:09:04 -0400

    updated model tuits with dislikes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -3,12 +3,14 @@ export default [
         "_id": "123",
         "postedBy": {"username": "NASA"},
         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
-        "likes": 345
+        "likes": 345,
+        "dislikes": 128
     },
     {
         "_id": "234",
         "postedBy": {"username": "NASA"},
         "tuit": "Ingenuity helicopter takes flight.",
-        "likes": 346785
+        "likes": 346785,
+        "dislikes": 27
     }
 ];
