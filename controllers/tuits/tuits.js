3ff56addd1f737a0b7ccfce9b4a82580f9b94e28
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-05 23:21:01 -0400

    Updated avatarImages to Node

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -2,6 +2,7 @@ export default [
     {
         "_id": "123",
         "postedBy": {"username": "NASA"},
+        "avatarImage": "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg",
         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
         "likes": 345,
         "dislikes": 128
@@ -9,6 +10,7 @@ export default [
     {
         "_id": "234",
         "postedBy": {"username": "NASA"},
+        "avatarImage": "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg",
         "tuit": "Ingenuity helicopter takes flight.",
         "likes": 346785,
         "dislikes": 27
