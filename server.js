151775d39ebe084749022272945f9162d3cd4d3d
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-08 17:19:28 -0400

    Updated to make Mongo Atlas Connectable

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitController from "./controllers/tuit-controller.js";
 
-mongoose.connect('mongodb://localhost:270127/webdev');
+//mongoose.connect('mongodb://localhost:270127/webdev');
+//Local first steps, now must implement Mongo Atlas for HW9
+const PROTOCOL = "mongodb+srv";
+//const DB_USERNAME = process.env.USERNAME;
+const DB_PASSWORD = process.env.PASSWORD;
+const HOST = "cluster0.tpmgl.mongodb.net";
+const DB_NAME = "webDev"
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://webDev:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+mongoose.connect(connectionString);
 
 const app = express();
 app.use(cors());
