6c17ede6e84837ff35a3108e1b0ec3867ae36d33
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-08 21:37:57 -0400

    Fixed NASA Avatar Image

--- mongoose/tuit-schema.js
+++ mongoose/tuit-schema.js
@@ -3,7 +3,7 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
-    avatarImage: {type: String, default: "https://open.spotify.com/track/3cKzY51GRiJngSVOnOfjgl?si=9d92fe6df3764e4e"},
+    avatarImage: {type: String, default: "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg"},
     postedBy: {
         username: {type: String, default: "NASA"}
     }
