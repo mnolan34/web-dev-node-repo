73b2f4fe7726a2d5fa2c69e150b5ab0a8a405c74
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-08 21:38:58 -0400

    One more try on the NASA logo

--- mongoose/tuit-schema.js
+++ mongoose/tuit-schema.js
@@ -3,7 +3,7 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
-    avatarImage: {type: String, default: "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg"},
+    avatarImage: {type: String, default: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png"},
     postedBy: {
         username: {type: String, default: "NASA"}
     }
