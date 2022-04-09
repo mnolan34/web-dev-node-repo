d9a031d3fc69884f9eb323f0194629f387219897
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-08 21:44:32 -0400

    Updated the NASA profile

--- mongoose/tuit-schema.js
+++ mongoose/tuit-schema.js
@@ -3,9 +3,9 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
-    avatarImage: {type: String, default: "https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2022/04/08101009/max-verstappen-red-bull-serious-pre-australian-grand-prix-2022-planetf1.jpg"},
+    avatarImage: {type: String, default: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png"},
     postedBy: {
-        username: {type: String, default: "Max Verstappen"}
+        username: {type: String, default: "NASA"}
     }
 }, {collection: 'tuits'});
 
