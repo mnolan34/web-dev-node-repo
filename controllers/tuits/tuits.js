158e2e7fffc37fe87eba66b947b66b4205397350
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-08 21:14:31 -0400

    Changed tuits.json to match the mongooseSchema

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -2,17 +2,13 @@ export default [
     {
         "_id": "123",
         "postedBy": {"username": "NASA"},
-        "avatarImage": "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg",
         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
-        "likes": 345,
-        "dislikes": 128
+        "likes": 345
     },
     {
         "_id": "234",
         "postedBy": {"username": "NASA"},
-        "avatarImage": "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg",
         "tuit": "Ingenuity helicopter takes flight.",
-        "likes": 346785,
-        "dislikes": 27
+        "likes": 346785
     }
 ];
--- dao/tuits-dao.js
+++ dao/tuits-dao.js
@@ -1,9 +1,8 @@
 import tuitsModel from "../mongoose/tuits-model.js";
 
-export default class tuitDao{
+export default class TuitDao{
     findAllTuits = () => tuitsModel.find();
     createTuit = (tuit) => tuitsModel.create(tuit);
     deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
     updateTuit = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit});
-
 }
